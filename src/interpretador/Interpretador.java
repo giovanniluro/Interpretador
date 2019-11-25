72e12c3b7320457b9aeda9ae144cb27cdca8bd4f
Giovanni Luro <giovanni.luro@outlook.com>
2019-11-25 14:15:48 -0300

    oi

---
+++ src/interpretador/Interpretador.java
@@ -0,0 +1,17 @@
+
+package interpretador;
+
+import java.io.IOException;
+
+public class Interpretador {
+
+  
+    public static void main(String[] args) throws IOException {
+        
+        InterpretadorUI n = new InterpretadorUI();
+        n.setVisible(true);
+        n.run();
+        
+    }
+    
+}
